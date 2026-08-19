0d649c6039cd87e5909fda804517cb73f583d92a
PrashanthBhisetti25 <prashanth.bhisetti.sde@gmail.com>
2026-08-19 07:10:41 +0000

    datatype

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Prashanth"); //-> it will not give an alert because we are using nodejs , not browser
+
+let name = "Prashanth";
+let age = 25;
+let isLoggedIn = true;
+
+//number  => 2 to power 53 
+// bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value / representation of empty value / it's an object you can say
+//undefined -> value not yet assigned / it's a type you can say
+//symbol -> to find uniqueness it will use
+
+//object -> 
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof null, typeof undefined]);
