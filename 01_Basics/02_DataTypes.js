5474be0b64f48ad0b885327334a35522d34d4567
PrashanthBhisetti25 <prashanth.bhisetti.sde@gmail.com>
2026-08-19 10:01:54 +0000

    Data Types summary

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -17,3 +17,48 @@ let isLoggedIn = true;
 //object -> 
 
 console.table([typeof name, typeof age, typeof isLoggedIn, typeof null, typeof undefined]);
+
+
+
+//summary of data types
+
+/*
+JS is dynamically typed language
+
+Types of datatypes
+1.Primitive   - call by value
+7 - types: String, Number, Boolean, null, undefined, Symbol,BigInt
+
+
+2.Non-primitive - call by reference (reference type)
+types: Arrays, Objects, Functions
+*/
+
+
+//1. primitive types
+let stdName = "Prashanth";
+let stdAge = 26;
+let isAdult = true;
+const outsideTemp = null;
+let myEmail;
+const id = Symbol('123');
+const aId = Symbol('123');
+const bigNumber = 2174184821748656544546n;
+console.table([stdName,stdAge,isAdult,outsideTemp,myEmail, id,aId,id == aId,bigNumber,typeof bigNumber]);
+
+//2.Non - Primitive Types - Array,Object,Function
+
+
+let heros = ["Spiderman","ironman","Thor","Hulk","Krrish"];
+let student = {
+    "name":"Prashanth",
+    "age":26,
+    isAdult : true
+}
+
+const myFun = function(){
+    return "It's a function";
+}
+
+console.table([typeof heros,typeof student,typeof myFun]);
+console.table([heros,student,myFun()]);
