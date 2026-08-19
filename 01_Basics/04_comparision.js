9a1084faa8b204e3fb333ca9c41ddcf62c4cc101
PrashanthBhisetti25 <prashanth.bhisetti.sde@gmail.com>
2026-08-19 09:34:04 +0000

    comparisions

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.table([2>3,4<5,2==5,2!=4,2==2,2<=3,4<=4]);
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+// console.log(undefined ==0);
+// console.log(undefined >0);
+// console.log(undefined <0);
+
+// strict check === and !==
+console.table(["2"==2,"3"!=3,2==="2","3"!==3,"3"==4]);
